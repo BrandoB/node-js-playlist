6c6befe79a7edf7ac94af5574febb4a274175b80
BrandoB <realbrandonb602@gmail.com>
2017-10-28 09:49:22 -0700

    Finished net ninja todo app

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,26 +1,51 @@
 // ToDo Controller
 var bodyParser = require('body-parser');
+
+var fs = require(`fs`);
+var connection = fs.readFileSync('connection.txt', 'utf8').trim();
+
+var mongoose = require('mongoose');
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [
-	{item: 'Get Milk'},
-	{item: 'Walk dog'},
-	{item: 'Kick some coding ass'}
-];
+//connect to the database
+mongoose.connect(connection);
+
+//Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+		item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+
+//var data = [
+//	{item: 'Walk dog'},
+//	{item: 'Kick some coding ass'}
+//];
 
 module.exports = function(app){
 
   app.get('/todo', function(req, res){
-  	res.render('todo', {todos: data});
+//get datat from mongodb abd pass ity to view
+		Todo.find({}, function(err, data){
+			if (err) throw err;
+			res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', urlencodedParser, function(req, res){
-		data.push(req.body);
-    console.log(data);
-		res.json({todos: data});
+		//get data from the view and add it to mongodb
+		var newTodo = Todo(req.body).save(function(err, data){
+			if (err) throw err;
+			res.json({todos: data});
+		});
  	});
 
-  app.delete('/todo', function(req, res){
-
+  app.delete('/todo/:item', function(req, res){
+		// delet the requested item from mongodb
+		Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+			if (err) throw err;
+			res.json(data);
+		});
   });
 };
